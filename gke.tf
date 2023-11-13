e16b2139fef8319f843f2c9302ce7a33e3edc147
Sean Lee <sean@vessl.ai>
2023-11-13 17:37:00 +0900

    First commit

---
+++ gke.tf
@@ -0,0 +1,35 @@
+resource "google_container_cluster" "this" {
+  network  = var.network_name
+  name     = var.cluster_name
+  location = var.region
+
+  min_master_version       = data.google_container_engine_versions.gke_version.latest_master_version
+  initial_node_count       = 0
+  remove_default_node_pool = true
+
+  private_cluster_config {
+    enable_private_nodes = true
+  }
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  addons_config {
+    gce_persistent_disk_csi_driver_config {
+      enabled = true
+    }
+    gcs_fuse_csi_driver_config {
+      enabled = true
+    }
+  }
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = var.secondary_ip_range_pods
+    services_secondary_range_name = var.secondary_ip_range_services
+  }
+
+  logging_service = "none"
+}
