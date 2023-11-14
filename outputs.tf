dd2acbef200df9e529567366741388ed718dabed
Sean Lee <sean@vessl.ai>
2023-11-14 16:26:21 +0900

    Add outputs

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "cluster_name" {
+  value = google_container_cluster.this.name
+}
+output "endpoint" {
+  value = "https://${google_container_cluster.this.endpoint}"
+}
+output "ca_certificate" {
+  value = base64decode(google_container_cluster.this.master_auth.0.cluster_ca_certificate)
+}
+output "kubernetes_version" {
+  value = split("-", google_container_cluster.this.master_version)[0]
+}
